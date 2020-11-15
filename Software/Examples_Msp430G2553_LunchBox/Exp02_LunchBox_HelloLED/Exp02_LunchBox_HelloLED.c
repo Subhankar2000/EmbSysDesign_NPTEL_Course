abd2572feb397b016ab4af1484cb8b82e5c8480e
Subhankar Karmakar <subhu990@gmail.com>
2020-11-15 20:33:55 +0530

    Update Exp02_LunchBox_HelloLED.c

--- Software/Examples_Msp430G2553_LunchBox/Exp02_LunchBox_HelloLED/Exp02_LunchBox_HelloLED.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp02_LunchBox_HelloLED/Exp02_LunchBox_HelloLED.c
@@ -1,18 +1,19 @@
 #include <msp430.h>
 
 /*@brief entry point for the code*/
-int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;       //! Stop Watchdog (Not recommended for code in production and devices working in field)
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD; //! Stop Watchdog (Not recommended for code in production and devices working in field)
 
     //P1DIR |= BIT7;
     P1DIR |= 0x80;                  // Set P1.7 to output direction
 
     //P1OUT |= BIT7;
-      P1OUT |= 0x80;                  // Set P1.7 to HIGH voltage
-                                      // As the LED is in Active High configuration physically on board, so setting the pin to HIGH will make it glow
+    P1OUT |= 0x80;                  // Set P1.7 to HIGH voltage
+                                    // As the LED is in Active High configuration physically on board, so setting the pin to HIGH will make it glow
 
     //P1OUT &=~ BIT7;
     //P1OUT &=~ 0x80;                  // Set P1.7 to LOW voltage
-                                      // As the LED is in Active High configuration physically on board, so setting the pin to LOW will make it OFF
+    // As the LED is in Active High configuration physically on board, so setting the pin to LOW will make it OFF
     return 0;
 }
