7d27dc12b83669319ae4a1fcd9b7e2502a49c552
Subhankar Karmakar <subhu990@gmail.com>
2020-11-15 20:24:29 +0530

    Update Exp02_LunchBox_HelloLED.c

--- Software/Examples_Msp430G2553_LunchBox/Exp02_LunchBox_HelloLED/Exp02_LunchBox_HelloLED.c
+++ Software/Examples_Msp430G2553_LunchBox/Exp02_LunchBox_HelloLED/Exp02_LunchBox_HelloLED.c
@@ -8,10 +8,11 @@ int main(void) {
     P1DIR |= 0x80;                  // Set P1.7 to output direction
 
     //P1OUT |= BIT7;
-   //P1OUT |= 0x80;                  // Set P1.7 to HIGH voltage
+      P1OUT |= 0x80;                  // Set P1.7 to HIGH voltage
+                                      // As the LED is in Active High configuration physically on board, so setting the pin to HIGH will make it glow
 
     //P1OUT &=~ BIT7;
-    P1OUT &=~ 0x80;                  // Set P1.7 to LOW voltage
-
+    //P1OUT &=~ 0x80;                  // Set P1.7 to LOW voltage
+                                      // As the LED is in Active High configuration physically on board, so setting the pin to LOW will make it OFF
     return 0;
 }
